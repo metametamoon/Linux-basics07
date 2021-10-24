580f7a31f677f5aedac9c7b4e196f8d72e1229ab
James Smith <js7572305@gmail.com>
2021-10-24 18:59:31 +0300

    ok36

--- .github/scripts/issus.sh
+++ .github/scripts/issus.sh
@@ -1,4 +1,3 @@
-gh issue list --search "Task 05" && (issNum=$(gh issue list --search "Task 05" | awk '{print $1}' \
-| sed s/.\(.*\)/\1/ )) \
+gh issue list --search "Task 05" && (issNum=$(gh issue list --search "Task 05" | awk '{print $1}')) \
 && gh issue comment $issNum --body "Yet another push" \
 || gh issue create --title "Task 05" --body "@sloboeugen98"
